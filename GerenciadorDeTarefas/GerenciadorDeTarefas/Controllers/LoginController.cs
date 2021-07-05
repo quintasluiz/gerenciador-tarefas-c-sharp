cf710491b069e72cf2d01b40e18f63ee5e5be806
Luiz Guilherme <79282765+quintasluiz@users.noreply.github.com>
2021-07-05 14:04:36 -0300

    primeira parte - projeto

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using GerenciadorDeTarefas.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpPost]
+        public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto requisicao)
+        {
+            try
+            {
+                throw new ArgumentException("Dados obrigatorios nao foram enviados");
+            }
+            catch(Exception excecao)
+            {
+                _logger.LogError("Ocorreu erro ao efetuar login", excecao, requisicao);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespostaDto() { 
+                    Status = StatusCodes.Status500InternalServerError,
+                    Erro = "Ocorreu erro ao efetuar login tente novamente"
+                });
+
+            }
+        }
+    }
+}
